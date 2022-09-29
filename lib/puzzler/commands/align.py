03e9fafe430b279d93fd35d0f3d894410feb04ad
Telemattic <matthew.eldridge@gmail.com>
2022-09-29 08:06:15 -0700

    Create a render context to deal with drawing more easily

--- lib/puzzler/commands/align.py
+++ lib/puzzler/commands/align.py
@@ -6,6 +6,52 @@ import PySimpleGUI as sg
 import scipy
 import puzzler
 
+class RenderContext:
+
+    def __init__(self):
+        self.stack  = []
+        self.matrix = np.identity(3)
+
+    def push(self):
+        self.stack.append(self.matrix)
+
+    def pop(self):
+        self.matrix = self.stack.pop()
+
+    def multiply(self, m):
+        self.matrix = self.matrix @ m
+
+    def scale(self, s):
+        m = np.array(((s, 0, 0),
+                      (0, s, 0),
+                      (0, 0, 1)))
+        self.multiply(m)
+
+    def translate(self, x, y):
+        m = np.array(((1, 0, x),
+                      (0, 1, y),
+                      (0, 0, 1)))
+        self.multiply(m)
+        
+    def rotate(self, rad):
+        c, s = np.cos(rad), np.sin(rad)
+        m = np.array(((c, -s, 0),
+                      (s,  c, 0),
+                      (0,  0, 1)))
+        self.multiply(m)
+
+    def to_v3(self, pts, w):
+        assert pts.ndim == 2 and pts.shape[1] == 2
+        return np.hstack((pts, np.full((len(pts),1), w, dtype=pts.dtype)))
+
+    def to_v2(self, pts):
+        assert pts.ndim == 2 and pts.shape[1] == 3
+        return pts[:,:2]
+
+    def to_device(self, pts):
+        return self.to_v2(self.to_v3(pts, 1) @ self.matrix.T)
+    
+
 class AffineTransform:
 
     def __init__(self, angle=0., xy=(0.,0.)):
@@ -225,9 +271,12 @@ class AlignUI:
         for i, p in enumerate(self.pieces):
 
             color = colors[i%len(colors)]
-            dxdy  = p.coords.dxdy
-            
-            points = np.squeeze(p.points) @ p.coords.rot_matrix() + dxdy
+
+            c = RenderContext()
+            c.translate(*p.coords.dxdy)
+            c.rotate(p.coords.angle)
+            points = c.to_device(p.points)
+                
             graph.draw_lines(points, color=color, width=2)
 
             h = DragHandle(p.coords)
