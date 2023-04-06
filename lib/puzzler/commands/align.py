c40a025b94efd3e27e7fc220c6390a21497f522e
Telemattic <matthew.eldridge@gmail.com>
2023-04-06 09:45:27 -0700

    UI to show tab alignment, new command to output tab alignment

--- lib/puzzler/commands/align.py
+++ lib/puzzler/commands/align.py
@@ -287,6 +287,11 @@ class PuzzleRenderer:
                     pts = puzzler.geometry.get_ellipse_points(tab.ellipse, npts=40)
                     r.draw_polygon(pts, fill='cyan', outline='')
 
+            if p.piece.tabs and False:
+                for i in range(len(p.piece.tabs)):
+                    lcr = puzzler.align.TabAligner.get_tab_points(p.piece, i)
+                    r.draw_points(lcr, fill='purple', radius=4)
+
             if self.render_fast:
                 points = p.perimeter.points[p.approx.indexes]
             else:
@@ -470,45 +475,6 @@ class AlignTk:
 
     def do_tab_alignment(self):
 
-        print("Tab alignment!")
-
-        num_indents = 0
-        num_outdents = 0
-        for p in self.pieces:
-            for t in p.piece.tabs:
-                if t.indent:
-                    num_indents += 1
-                else:
-                    num_outdents += 1
-        
-        print(f"{len(self.pieces)} pieces: {num_indents} indents, {num_outdents} outdents")
-
-        with open('tab_alignment.csv', 'w', newline='') as f:
-            field_names = 'dst_label dst_tab_no src_label src_tab_no mse'.split()
-            writer = csv.DictWriter(f, field_names)
-            writer.writeheader()
-
-            for dst in self.pieces:
-                rows = []
-                tab_aligner = puzzler.align.TabAligner(dst.piece)
-                for src in self.pieces:
-                    if src is dst:
-                        continue
-                    for dst_tab_no, dst_tab in enumerate(dst.piece.tabs):
-                        for src_tab_no, src_tab in enumerate(src.piece.tabs):
-                            if dst_tab.indent == src_tab.indent:
-                                continue
-                            mse = tab_aligner.compute_alignment(dst_tab_no, src.piece, src_tab_no)[0]
-                            rows.append({'dst_label': dst.piece.label,
-                                         'dst_tab_no': dst_tab_no,
-                                         'src_label': src.piece.label,
-                                         'src_tab_no': src_tab_no,
-                                         'mse': mse})
-                print(f"{dst.piece.label}: {len(rows)} rows")
-                writer.writerows(rows)
-
-    def do_tab_alignment_B2(self):
-
         self.solver.solve_field()
         self.update_coords()
                     
@@ -516,7 +482,7 @@ class AlignTk:
 
         if self.var_solve_continuous.get():
             if self.solver.corners:
-                self.parent.after_idle(self.do_tab_alignment_B2)
+                self.parent.after_idle(self.do_tab_alignment)
             else:
                 self.var_solve_continuous.set(0)
 
@@ -583,7 +549,7 @@ class AlignTk:
         b1 = ttk.Button(self.controls, text='Solve!', command=self.do_solve)
         b1.grid(column=0, row=0, sticky=W)
 
-        b2 = ttk.Button(self.controls, text='Tab Alignment', command=self.do_tab_alignment_B2)
+        b2 = ttk.Button(self.controls, text='Tab Alignment', command=self.do_tab_alignment)
         b2.grid(column=1, row=0, sticky=W)
 
         self.var_render_adjacency = IntVar(value=1)
@@ -599,7 +565,44 @@ class AlignTk:
         l1 = ttk.Label(self.controls, textvariable=self.var_label, width=80)
         l1.grid(column=4, row=0, sticky=(E))
 
+        cf2 = ttk.Frame(self.frame)
+        cf2.grid(row=2, sticky=(W,E))
+
+        b5 = ttk.Button(cf2, text='Show Tab Alignment', command=self.show_tab_alignment)
+        b5.grid(column=0, row=0, sticky=W)
+
+        self.var_show_tab_alignment = StringVar(value='')
+        e1 = ttk.Entry(cf2, width=16, textvariable=self.var_show_tab_alignment)
+        e1.grid(column=1, row=0)
+
         self.render_full = False
+        self.render()
+
+    def show_tab_alignment(self):
+        s = self.var_show_tab_alignment.get().strip()
+        m = re.fullmatch("([a-zA-Z]+\d+):(\d+)-([a-zA-Z]+\d+):(\d+)", s)
+        if not m:
+            print(f"bad input: \"{s}\"")
+            return
+        
+        dst_label = m[1]
+        dst_tab_no = int(m[2])
+        src_label = m[3]
+        src_tab_no = int(m[4])
+        
+        print(f"{dst_label=} {dst_tab_no=} {src_label=} {src_tab_no=}")
+
+        pieces = dict([(i.piece.label, i) for i in self.pieces])
+        dst = pieces[dst_label]
+        src = pieces[src_label]
+        
+        tab_aligner = puzzler.align.TabAligner(dst.piece)
+        mse, src_coords, sfp, dfp = tab_aligner.compute_alignment(dst_tab_no, src.piece, src_tab_no)
+        print(f"{mse=} {src_coords=} {sfp=} {dfp=}")
+
+        dst.coords = puzzler.align.AffineTransform(0., (0., 2000.))
+        src.coords = puzzler.align.AffineTransform(src_coords.angle, src_coords.dxdy + dst.coords.dxdy)
+
         self.render()
                 
 def align_ui(args):
@@ -635,24 +638,25 @@ def align_ui(args):
     def to_col(s):
         return int(s)
 
+    def to_row_col(label):
+        m = re.fullmatch("([a-zA-Z]+)(\d+)", label)
+        return (to_row(m[1]), to_col(m[2])) if m else (None, None)
+
     rows = set()
     cols = set()
     for piece in pieces:
-        m = re.fullmatch("([a-zA-Z]+)(\d+)", piece.piece.label)
-        if m:
-            rows.add(to_row(m[1]))
-            cols.add(to_col(m[2]))
+        r, c = to_row_col(piece.piece.label)
+        rows.add(r)
+        cols.add(c)
 
     rows = dict((r, i) for i, r in enumerate(sorted(rows)))
     cols = dict((c, i) for i, c in enumerate(sorted(cols)))
 
     for piece in pieces:
-        m = re.fullmatch("([a-zA-Z]+)(\d+)", piece.piece.label)
-        if m:
-            row, col = to_row(m[1]), to_col(m[2])
-            x = cols[col] * 1000.
-            y = rows[row] * -1000.
-            piece.coords.dxdy = np.array((x, y))
+        r, c = to_row_col(piece.piece.label)
+        x = cols[c] * 1000.
+        y = rows[r] * -1000.
+        piece.coords.dxdy = np.array((x, y))
 
     root = Tk()
     ui = AlignTk(root, pieces)
@@ -665,6 +669,91 @@ def align_ui(args):
     
     root.mainloop()
 
+def output_tabs(args):
+
+    puzzle = puzzler.file.load(args.puzzle)
+
+    print("Tab alignment!")
+
+    def to_row(s):
+        row = 0
+        for i in s.upper():
+            row *= 26
+            row += ord(i) + 1 - ord('A')
+        return row
+
+    def to_col(s):
+        return int(s)
+
+    def to_row_col(label):
+        m = re.fullmatch("([a-zA-Z]+)(\d+)", label)
+        return (to_row(m[1]), to_col(m[2])) if m else (None, None)
+
+    def sort_key(piece):
+        row, col = to_row_col(piece.label)
+        return (row, col, piece.label)
+
+    pieces = sorted([i for i in puzzle.pieces], key=sort_key)
+    
+    num_indents = 0
+    num_outdents = 0
+    for p in pieces:
+        for t in p.tabs:
+            if t.indent:
+                num_indents += 1
+            else:
+                num_outdents += 1
+    
+    print(f"{len(pieces)} pieces: {num_indents} indents, {num_outdents} outdents")
+
+    with open(args.output, 'w', newline='') as f:
+        field_names = 'dst_label dst_tab_no dst_col_no dst_row_no src_label src_tab_no src_col_no src_row_no src_coord_x src_coord_y src_coord_angle src_index_0 src_index_1 mse neighbor'.split()
+        writer = csv.DictWriter(f, field_names)
+        writer.writeheader()
+
+        for dst in pieces:
+            rows = []
+            tab_aligner = puzzler.align.TabAligner(dst)
+            for src in pieces:
+                if src is dst:
+                    continue
+                for dst_tab_no, dst_tab in enumerate(dst.tabs):
+                    for src_tab_no, src_tab in enumerate(src.tabs):
+                        if dst_tab.indent == src_tab.indent:
+                            continue
+                        dst_row_no, dst_col_no = to_row_col(dst.label)
+                        src_row_no, src_col_no = to_row_col(src.label)
+                        neighbor = None
+                        if dst_row_no == src_row_no:
+                            if dst_col_no == src_col_no - 1:
+                                neighbor = 'E'
+                            if dst_col_no == src_col_no + 1:
+                                neighbor = 'W'
+                        elif dst_col_no == src_col_no:
+                            if dst_row_no == src_row_no - 1:
+                                neighbor = 'S'
+                            if dst_row_no == src_row_no + 1:
+                                neighbor = 'N'
+                            
+                        mse, src_coords, sfp, dfp = tab_aligner.compute_alignment(dst_tab_no, src, src_tab_no)
+                        rows.append({'dst_label': dst.label,
+                                     'dst_tab_no': dst_tab_no,
+                                     'dst_col_no': dst_col_no,
+                                     'dst_row_no': dst_row_no,
+                                     'src_label': src.label,
+                                     'src_tab_no': src_tab_no,
+                                     'src_col_no': src_col_no,
+                                     'src_row_no': src_row_no,
+                                     'src_coord_x': src_coords.dxdy[0],
+                                     'src_coord_y': src_coords.dxdy[1],
+                                     'src_coord_angle': src_coords.angle,
+                                     'src_index_0': sfp[0],
+                                     'src_index_1': sfp[1],
+                                     'mse': mse,
+                                     'neighbor': neighbor})
+            print(f"{dst.label}: {len(rows)} rows")
+            writer.writerows(rows)
+
 def add_parser(commands):
 
     parser_align = commands.add_parser("align", help="UI to experiment with aligning pieces")
@@ -672,3 +761,7 @@ def add_parser(commands):
     parser_align.add_argument("-e", "--edges", help="add all edges", action='store_true')
     parser_align.add_argument("-g", "--geometry", help="geometry file")
     parser_align.set_defaults(func=align_ui)
+
+    parser_tabs = commands.add_parser("tabs", help="Output a CSV enumerating all possible tab matches")
+    parser_tabs.add_argument("-o", "--output", help="output csv path")
+    parser_tabs.set_defaults(func=output_tabs)
