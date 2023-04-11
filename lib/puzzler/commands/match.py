04149c90338256b51b81639cff9609f5f82649e8
Telemattic <matthew.eldridge@gmail.com>
2023-04-11 07:34:15 -0700

    Pull out the computation into a separate class

--- lib/puzzler/commands/match.py
+++ lib/puzzler/commands/match.py
@@ -9,6 +9,67 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 import matplotlib
 
+class Curvature:
+
+    def __init__(self, path, stepsize):
+
+        self.points = puzzler.potrace.InterpolatePath(stepsize).apply(path)
+        self.stepsize = stepsize
+        
+        self.turning_angle = self.compute_turning_angle(self.points)
+        self.cum_turning_angle = np.cumsum(self.turning_angle)
+        self.cum_path_length = np.arange(len(self.turning_angle)) * self.stepsize
+        self.curvature = self.compute_curvature(self.cum_path_length, self.cum_turning_angle)
+
+    def interp_point(self, cpl):
+        # print(f"interp_point: {cpl=} {self.cum_path_length=} {self.points=}")
+        x = cpl / self.stepsize
+        i = int(x)
+        n = len(self.points)
+        if i < 0:
+            return self.points[0]
+        if i >= n-1:
+            return self.points[n-1]
+
+        t = x - i
+        return (1 - t) * self.points[i] + t * self.points[i+1]
+
+    @staticmethod
+    def compute_turning_angle(points):
+        
+        turning_angle = []
+        
+        n = len(points)
+        for i in range(n):
+            p0 = points[i-1] if i > 0 else points[n-1]
+            p1 = points[i]
+            p2 = points[i+1] if i+1 < n else points[0]
+                
+            x0, y0 = p2 - p1
+            x1, y1 = p1 - p0
+            t  = np.arctan2(x0*y1 - y0*x1, x0*x1 + y0*y1)
+            turning_angle.append(t)
+
+        return np.array(turning_angle)
+
+    @staticmethod
+    def compute_curvature(path_length, cum_turning_angle):
+
+        retval = []
+        k = 5
+        ds = 50
+        smin = path_length.min()
+        smax = path_length.max()
+        s = smin
+        while s < smax:
+            samples = np.linspace(s+ds, s+ds+k, num=k)
+            c = (np.average(np.interp(samples, path_length, cum_turning_angle)) -
+                 np.interp(s, path_length, cum_turning_angle))
+            retval.append(c)
+            s += 1
+
+        return np.array(retval)
+
 class MatchTk:
 
     def __init__(self, parent, puzzle, labels):
@@ -84,32 +145,7 @@ class MatchTk:
         self.potrace_path = puzzler.potrace.piece_to_path(self.piece)
         self.render()
 
-        stepsize = self.var_stepsize.get()
-        points = puzzler.potrace.InterpolatePath(stepsize).apply(self.potrace_path)
-
-        kdtree = scipy.spatial.KDTree(self.piece.points)
-        ii = kdtree.query(points)[1]
-        
-        # n = len(points)
-        # turning_angle = []
-        # path_length = []
-        # for i in range(n):
-        #     p0 = points[i-1] if i > 0 else points[n-1]
-        #     p1 = points[i]
-        #     p2 = points[i+1] if i+1 < n else points[0]
-        #         
-        #     x0, y0 = p2 - p1
-        #     x1, y1 = p1 - p0
-        #     t  = np.arctan2(x0*y1 - y0*x1, x0*x1 + y0*y1)
-        #     turning_angle.append(t)
-        #     path_length.append(stepsize)
-
-        turning_angle = self.compute_turning_angle(points, stepsize)
-        
-        cum_path_length = np.arange(len(turning_angle)) * stepsize
-        cum_turning_angle = np.cumsum(turning_angle)
-
-        curvature = self.compute_curvature(cum_path_length, cum_turning_angle)
+        self.curvature = c = Curvature(self.potrace_path, self.var_stepsize.get())
 
         f = self.figure
         for ax in f.axes.copy():
@@ -119,62 +155,21 @@ class MatchTk:
         ax2 = f.add_subplot(3, 1, 2, sharex=ax1)
         ax3 = f.add_subplot(3, 1, 3, sharex=ax1)
         
-        ax1.plot(cum_path_length, turning_angle)
+        ax1.plot(c.cum_path_length, c.turning_angle)
         ax1.set_ylabel('turning angle')
         ax1.grid(True)
         
-        ax2.plot(cum_path_length, cum_turning_angle)
+        ax2.plot(c.cum_path_length, c.cum_turning_angle)
         ax2.set_ylabel('cum. turning angle')
         ax2.grid(True)
         
-        ax3.plot(np.arange(1,len(curvature)+1), curvature)
+        ax3.plot(np.arange(1,len(c.curvature)+1), c.curvature)
         ax3.set_ylabel('curvature')
         ax3.grid(True)
 
-        self.ii = ii
         self.mc = matplotlib.widgets.MultiCursor(self.figure_canvas, (ax1, ax2, ax3), color='blue', linestyle='--', linewidth=1.)
 
         self.figure_canvas.draw()
-
-    @staticmethod
-    def cum_path_length_to_point_no(x, cum_path_length, points):
-        return np.interp(cpl, cum_path_length, points)
-
-    @staticmethod
-    def compute_turning_angle(points, stepsize):
-        
-        turning_angle = []
-        
-        n = len(points)
-        for i in range(n):
-            p0 = points[i-1] if i > 0 else points[n-1]
-            p1 = points[i]
-            p2 = points[i+1] if i+1 < n else points[0]
-                
-            x0, y0 = p2 - p1
-            x1, y1 = p1 - p0
-            t  = np.arctan2(x0*y1 - y0*x1, x0*x1 + y0*y1)
-            turning_angle.append(t)
-
-        return np.array(turning_angle)
-
-    @staticmethod
-    def compute_curvature(path_length, cum_turning_angle):
-
-        retval = []
-        k = 5
-        ds = 50
-        smin = path_length.min()
-        smax = path_length.max()
-        s = smin
-        while s < smax:
-            samples = np.linspace(s+ds, s+ds+k, num=k)
-            c = (np.average(np.interp(samples, path_length, cum_turning_angle)) -
-                 np.interp(s, path_length, cum_turning_angle))
-            retval.append(c)
-            s += 1
-
-        return np.array(retval)
 
     def get_camera_matrix(self):
 
@@ -235,13 +230,10 @@ class MatchTk:
         if self.cursor_xdata is None:
             return
 
-        i = int(self.cursor_xdata / self.var_stepsize.get())
-        if 0 <= i < len(self.ii):
-            i = self.ii[i]
-            c = self.piece.points[i]
-            r = puzzler.renderer.canvas.CanvasRenderer(canvas)
-            r.transform(self.get_camera_matrix())
-            r.draw_circle(c, radius=6, fill='', outline='red', tag='cursor')
+        c = self.curvature.interp_point(self.cursor_xdata)
+        r = puzzler.renderer.canvas.CanvasRenderer(canvas)
+        r.transform(self.get_camera_matrix())
+        r.draw_circle(c, radius=6, fill='', outline='red', tag='cursor')
 
 def match(args):
 
