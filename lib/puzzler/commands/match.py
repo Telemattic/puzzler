6a253a1e40cb7f269e23e92ed5495f794ab03109
Telemattic <matthew.eldridge@gmail.com>
2023-04-12 06:14:21 -0700

    Continued implementation

--- lib/puzzler/commands/match.py
+++ lib/puzzler/commands/match.py
@@ -11,15 +11,29 @@ import matplotlib
 
 class Curvature:
 
-    def __init__(self, path, stepsize):
+    def __init__(self, path, stepsize, reverse=False, ds=None):
 
         self.points = puzzler.potrace.InterpolatePath(stepsize).apply(path)
+        if reverse:
+            self.points = np.flip(self.points, axis=0)
+        
         self.stepsize = stepsize
         
         self.turning_angle = self.compute_turning_angle(self.points)
         self.cum_turning_angle = np.cumsum(self.turning_angle)
         self.cum_path_length = np.arange(len(self.turning_angle)) * self.stepsize
-        self.curvature = self.compute_curvature(self.cum_path_length, self.cum_turning_angle)
+
+        if ds is not None:
+            # assert len(fir_filter) % 2 == 1
+            # n = len(fir_filter) // 2
+            # fcta = scipy.signal.lfilter(fir_filter, 1., self.cum_turning_angle, axis=0)
+            # # print(f"lfilter: len(x)={len(self.cum_turning_angle)} len(y)={len(fcta)}")
+            # self.cum_turning_angle = fcta
+
+            cta = self.cum_turning_angle
+            self.curvature = np.hstack((np.zeros(ds//2), cta[ds:] - cta[:-ds], np.zeros(ds//2)))
+        else:
+            self.curvature = self.compute_curvature(self.cum_path_length, self.cum_turning_angle)
 
     def interp_point(self, cpl):
         # print(f"interp_point: {cpl=} {self.cum_path_length=} {self.points=}")
@@ -70,6 +84,36 @@ class Curvature:
 
         return np.array(retval)
 
+def algorithm_ii(curve_a, curve_b, eps=0.2):
+    
+    index_a = np.argsort(curve_a)
+    
+    index_b = np.argsort(curve_b)
+    from_ = to = 0
+    n = len(index_b)
+
+    count = np.zeros(len(curve_a) + len(curve_b), dtype=np.int32)
+
+    for i in index_a:
+        while from_ < n and curve_a[i]-eps > curve_b[index_b[from_]]:
+            from_ += 1
+        while to < n and curve_a[i]+eps > curve_b[index_b[to]]:
+            to += 1
+
+        for j in range(from_,to):
+            count[i-index_b[j]+n] += 1
+
+    print(f"max count at i-j={np.argmax(count)-n}, total counts={sum(count)}")
+
+    peaks, _ = scipy.signal.find_peaks(count, distance=150)
+    highest = np.max(count[peaks])
+
+    for p in peaks:
+        if count[p] > .5 * highest:
+            print(f"peak: offset={p-n}, count={count[p]}")
+            
+    return count
+
 class MatchTk:
 
     def __init__(self, parent, puzzle, labels):
@@ -81,6 +125,7 @@ class MatchTk:
         assert piece is not None
         
         self.puzzle = puzzle
+        self.labels = labels
         self.piece = piece
 
         self.frame = ttk.Frame(parent, padding=5)
@@ -97,7 +142,7 @@ class MatchTk:
 
         self.figure_canvas = FigureCanvasTkAgg(self.figure, master=parent)
         self.figure_canvas.draw()
-        self.figure_canvas.get_tk_widget().grid(column=1, row=0, sticky=(tkinter.N, tkinter.E, tkinter.S))
+        self.figure_canvas.get_tk_widget().grid(column=1, row=0, sticky=(tkinter.N, tkinter.E, tkinter.S, tkinter.W))
         self.figure_canvas.mpl_connect('button_press_event', self.on_click)
         self.figure_canvas.mpl_connect('motion_notify_event', self.on_motion)
 
@@ -106,30 +151,50 @@ class MatchTk:
 
         ttk.Button(self.controls, text='potrace', command=self.potrace).grid(column=0, row=0)
 
-        ttk.Label(self.controls, text='Epsilon').grid(column=1, row=0)
+        self.var_label = tkinter.StringVar(value=labels[0])
+        cb = ttk.Combobox(self.controls, textvariable=self.var_label, state='readonly', values=labels, width=8)
+        cb.grid(column=1, row=0)
+        cb.bind("<<ComboboxSelected>>", self.on_select_piece)
+
+        ttk.Label(self.controls, text='Stepsize').grid(column=2, row=0)
 
         self.var_stepsize = tkinter.DoubleVar(value=1.)
-        ttk.Entry(self.controls, width=8, textvariable=self.var_stepsize).grid(column=2, row=0)
+        ttk.Entry(self.controls, width=8, textvariable=self.var_stepsize).grid(column=3, row=0)
 
-        self.var_render_potrace_path = tkinter.IntVar(value=1)
+        self.var_render_potrace_path = tkinter.IntVar(value=0)
         ttk.Checkbutton(self.controls, text='Path', command=self.render,
-                        variable=self.var_render_potrace_path).grid(column=3, row=0)
+                        variable=self.var_render_potrace_path).grid(column=4, row=0)
 
-        self.var_render_potrace_points = tkinter.IntVar(value=0)
+        self.var_render_potrace_points = tkinter.IntVar(value=1)
         ttk.Checkbutton(self.controls, text='Points', command=self.render,
-                        variable=self.var_render_potrace_points).grid(column=4, row=0)
+                        variable=self.var_render_potrace_points).grid(column=5, row=0)
 
         self.var_render_potrace_lines = tkinter.IntVar(value=0)
         ttk.Checkbutton(self.controls, text='Lines', command=self.render,
-                        variable=self.var_render_potrace_lines).grid(column=5, row=0)
+                        variable=self.var_render_potrace_lines).grid(column=6, row=0)
 
         self.var_render_perimeter = tkinter.IntVar(value=1)
         ttk.Checkbutton(self.controls, text='Perimeter', command=self.render,
-                        variable=self.var_render_perimeter).grid(column=6, row=0)
+                        variable=self.var_render_perimeter).grid(column=7, row=0)
+
+        self.var_ds = tkinter.IntVar(value=50)
+        ttk.Label(self.controls, text='ds').grid(column=8, row=0)
+        ttk.Entry(self.controls, width=5, textvariable=self.var_ds).grid(column=9, row=0)
+
+        self.var_epsilon = tkinter.DoubleVar(value=0.2)
+        ttk.Label(self.controls, text='eps').grid(column=10, row=0)
+        ttk.Entry(self.controls, width=5, textvariable=self.var_epsilon).grid(column=11, row=0)
+
+        self.var_offset = tkinter.IntVar(value=0)
+        ttk.Label(self.controls, text='offset').grid(column=12, row=0)
+        ttk.Entry(self.controls, width=5, textvariable=self.var_offset).grid(column=13, row=0)
 
         self.potrace_path = None
         self.cursor_xdata = None
         self.render()
+
+    def on_select_piece(self, event):
+        print(f"on_select_piece: {self.var_label.get()}")
 
     def on_click(self, event):
         e = event
@@ -145,29 +210,57 @@ class MatchTk:
         self.potrace_path = puzzler.potrace.piece_to_path(self.piece)
         self.render()
 
-        self.curvature = c = Curvature(self.potrace_path, self.var_stepsize.get())
+        ds = self.var_ds.get()
+        self.curvature = c = Curvature(self.potrace_path, self.var_stepsize.get(), ds=ds)
+
+        other_label = self.labels[1]
+        other_piece = None
+        for p in self.puzzle.pieces:
+            if p.label == other_label:
+                other_piece = p
+                break
+        assert other_piece is not None
+
+        other_potrace_path = puzzler.potrace.piece_to_path(other_piece)
+        c2 = Curvature(other_potrace_path, self.var_stepsize.get(), reverse=True, ds=ds)
+
+        eps = self.var_epsilon.get()
+        correlation = algorithm_ii(c.curvature, c2.curvature, eps=eps)
+        lags = np.arange(-len(c2.curvature),len(c.curvature))
 
         f = self.figure
         for ax in f.axes.copy():
             f.delaxes(ax)
             
         ax1 = f.add_subplot(3, 1, 1)
-        ax2 = f.add_subplot(3, 1, 2, sharex=ax1)
-        ax3 = f.add_subplot(3, 1, 3, sharex=ax1)
-        
-        ax1.plot(c.cum_path_length, c.turning_angle)
-        ax1.set_ylabel('turning angle')
+        ax2 = f.add_subplot(3, 1, 2)
+        ax3 = f.add_subplot(3, 1, 3)
+
+        ax1.plot(np.arange(1,len(c.curvature)+1), c.curvature, np.arange(1,len(c2.curvature)+1), c2.curvature)
+        ax1.set_ylabel('curvature')
         ax1.grid(True)
-        
-        ax2.plot(c.cum_path_length, c.cum_turning_angle)
-        ax2.set_ylabel('cum. turning angle')
+
+        ax2.plot(lags, correlation)
+        ax2.set_ylabel('correlation')
+        ax2.set_xlabel('offset')
         ax2.grid(True)
-        
-        ax3.plot(np.arange(1,len(c.curvature)+1), c.curvature)
-        ax3.set_ylabel('curvature')
+
+        c1 = c.curvature
+        c2 = c2.curvature
+        offset = self.var_offset.get()
+        if offset < 0:
+            start1, end1 = 0, min(len(c1), len(c2)+offset)
+        else:
+            start1, end1 = offset, min(len(c1)-offset, len(c2))
+            
+        start2, end2 = start1-offset, end1-offset
+
+        c3 = np.abs(c1[start1:end1] - c2[start2:end2])
+        ax3.plot(np.arange(start1,end1), c3)
+        ax3.set_ylabel('abs(A - B)')
         ax3.grid(True)
 
-        self.mc = matplotlib.widgets.MultiCursor(self.figure_canvas, (ax1, ax2, ax3), color='blue', linestyle='--', linewidth=1.)
+        # self.mc = matplotlib.widgets.MultiCursor(self.figure_canvas, (ax1, ax2, ax3), color='blue', linestyle='--', linewidth=1.)
 
         self.figure_canvas.draw()
 
