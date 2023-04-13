3aad36e58db77a44dc8cc57468f8363b8e5bdca6
Telemattic <matthew.eldridge@gmail.com>
2023-04-13 07:02:17 -0700

    Many changes

--- lib/puzzler/commands/match.py
+++ lib/puzzler/commands/match.py
@@ -1,3 +1,4 @@
+import math
 import numpy as np
 import puzzler
 import scipy
@@ -114,19 +115,113 @@ def algorithm_ii(curve_a, curve_b, eps=0.2):
             
     return count
 
+class Camera:
+
+    def __init__(self, center, zoom, viewport):
+        self._center = center.copy()
+        self._zoom = zoom
+        self._viewport = viewport
+        self._matrix = None
+        self.__update_matrix()
+
+    @property
+    def center(self):
+        return self._center
+
+    @center.setter
+    def center(self, v):
+        self._center = v.copy()
+        self.__update_matrix()
+
+    @property
+    def zoom(self):
+        return self._zoom
+
+    @zoom.setter
+    def zoom(self, v):
+        self._zoom = v
+        self.__update_matrix()
+
+    @property
+    def viewport(self):
+        return self._viewport
+
+    @viewport.setter
+    def viewport(self, v):
+        self._viewport = v
+        self.__update_matrix()
+
+    @property
+    def matrix(self):
+        return self._matrix
+
+    def fixed_point_zoom(self, f, xy):
+        xy  = np.array((*xy, 1))
+        inv = np.linalg.inv(self._matrix)
+        xy  = (xy @ inv.T)[:2]
+
+        self._center = self._zoom * (f - 1) * xy + self._center
+        self._zoom   = self._zoom * f
+        self.__update_matrix()
+
+    def __update_matrix(self):
+        w, h = self.viewport
+        
+        vp = np.array(((1,  0, w/2),
+                       (0, -1, h/2),
+                       (0,  0, 1)), dtype=np.float64)
+
+        x, y = self.center
+        z = self.zoom
+        
+        lookat = np.array(((z, 0, -x),
+                           (0, z, -y),
+                           (0, 0,  1)))
+
+        self._matrix = vp @ lookat
+
+class Draggable:
+
+    def start(self, xy):
+        pass
+
+    def drag(self, xy):
+        raise NotImplementedError
+
+    def commit(self):
+        pass
+
+class MoveCamera(Draggable):
+
+    def __init__(self, camera):
+        self.camera = camera
+        self.init_camera_center = camera.center.copy()
+
+    def start(self, xy):
+        self.origin = xy
+
+    def drag(self, xy):
+        delta = (xy - self.origin) * np.array((-1, 1))
+        self.camera.center = self.init_camera_center + delta
+
 class MatchTk:
 
     def __init__(self, parent, puzzle, labels):
 
-        piece = None
+        pieces = dict()
+        coords = dict()
         for p in puzzle.pieces:
-            if p.label == labels[0]:
-                piece = p
-        assert piece is not None
+            if p.label in labels:
+                x = 1000. * len(pieces)
+                y = 0.
+                pieces[p.label] = p
+                coords[p.label] = puzzler.align.AffineTransform(0., (x,y))
         
         self.puzzle = puzzle
         self.labels = labels
-        self.piece = piece
+        self.pieces = pieces
+        self.coords = coords
+        self.curves = dict()
 
         self.frame = ttk.Frame(parent, padding=5)
         self.frame.grid(column=0, row=0, sticky=(tkinter.N, tkinter.W, tkinter.E, tkinter.S))
@@ -136,6 +231,10 @@ class MatchTk:
         self.puzzle_canvas = tkinter.Canvas(self.frame, width=800, height=800,
                              background='white', highlightthickness=0)
         self.puzzle_canvas.grid(column=0, row=0, sticky=(tkinter.N, tkinter.W, tkinter.S))
+        self.puzzle_canvas.bind("<Button-1>", self.on_puzzle_press)
+        self.puzzle_canvas.bind("<B1-Motion>", self.on_puzzle_drag)
+        self.puzzle_canvas.bind("<ButtonRelease-1>", self.on_puzzle_release)
+        self.puzzle_canvas.bind("<MouseWheel>", self.on_puzzle_wheel)
 
         # https://matplotlib.org/stable/gallery/user_interfaces/embedding_in_tk_sgskip.html
         self.figure = Figure(figsize=(8,8), dpi=100, facecolor='white')
@@ -143,8 +242,8 @@ class MatchTk:
         self.figure_canvas = FigureCanvasTkAgg(self.figure, master=parent)
         self.figure_canvas.draw()
         self.figure_canvas.get_tk_widget().grid(column=1, row=0, sticky=(tkinter.N, tkinter.E, tkinter.S, tkinter.W))
-        self.figure_canvas.mpl_connect('button_press_event', self.on_click)
-        self.figure_canvas.mpl_connect('motion_notify_event', self.on_motion)
+        self.figure_canvas.mpl_connect('button_press_event', self.on_figure_press)
+        self.figure_canvas.mpl_connect('motion_notify_event', self.on_figure_motion)
 
         self.controls = ttk.Frame(self.frame)
         self.controls.grid(column=0, row=1, sticky=(tkinter.N, tkinter.W, tkinter.E, tkinter.S), pady=5)
@@ -187,46 +286,141 @@ class MatchTk:
 
         self.var_offset = tkinter.IntVar(value=0)
         ttk.Label(self.controls, text='offset').grid(column=12, row=0)
-        ttk.Entry(self.controls, width=5, textvariable=self.var_offset).grid(column=13, row=0)
+        ttk.Entry(self.controls, width=8, textvariable=self.var_offset).grid(column=13, row=0)
+
+        ttk.Button(self.controls, text='align', command=self.align).grid(column=0, row=1)
+
+        scale = ttk.Scale(self.controls, from_=-2000, to=1999, length=500,
+                          orient=tkinter.HORIZONTAL, variable=self.var_offset, command=self.on_offset_update)
+        scale.grid(column=1,row=1,columnspan=12)
 
         self.potrace_path = None
         self.cursor_xdata = None
+        self.draggable = None
+        self.dst_points = [None, None]
+        self.src_points = [None, None]
+        self.camera = Camera(np.array((0,0), dtype=np.float64), 1/3, (800,800))
+        self.render()
+
+    def on_offset_update(self, x):
+        # print(f"offset: {self.var_offset.get()} {x=}")
+
+        axes = self.figure.axes
+        if len(axes) != 3:
+            return
+        
+        ax1, ax2, ax3 = axes
+        
+        offset = self.var_offset.get()
+
+        c1 = self.curves[self.labels[0]].curvature
+        c2 = self.curves[self.labels[1]].curvature
+        start1, end1 = max(0, offset), min(len(c1), len(c2)+offset)
+        start2, end2 = start1-offset, end1-offset
+
+        ax1.lines[1].set_xdata(np.arange(1,len(c2)+1)+offset)
+
+        c3 = np.abs(c1[start1:end1] - c2[start2:end2])
+        ax3.lines[0].set_data(np.arange(start1, end1), c3)
+        
+        self.figure_canvas.draw_idle()
+
+    def align(self):
+        print("Align!")
+        print(f"dst_points={self.dst_points} src_points={self.src_points}")
+
+        dst_curve = self.curves[self.labels[0]]
+        src_curve = self.curves[self.labels[1]]
+
+        dst_piece = self.pieces[self.labels[0]]
+        src_piece = self.pieces[self.labels[1]]
+
+        dst_points = dst_curve.points[self.dst_points]
+        src_points = src_curve.points[self.src_points]
+
+        print(f"{dst_points=} {src_points=}")
+
+        dst_vec = dst_points[1] - dst_points[0]
+        dst_angle = np.arctan2(dst_vec[1], dst_vec[0])
+
+        src_vec = src_points[1] - src_points[0]
+        src_angle = np.arctan2(src_vec[1], src_vec[0])
+
+        print(f"dst_angle={math.degrees(dst_angle):.1f} src_angle={math.degrees(src_angle):.1f}")
+
+        src_points_rotated = puzzler.align.AffineTransform(dst_angle-src_angle).get_transform().apply_v2(src_points)
+        r, x, y = puzzler.align.compute_rigid_transform(src_points_rotated, dst_points)
+        print(f"rigid_transform: {r=:.3f} {x=:.1f} {y=:.1f}")
+        r += dst_angle - src_angle
+
+        self.coords[self.labels[1]] = puzzler.align.AffineTransform(r, (x,y))
+        self.render()
+
+    def on_puzzle_press(self, event):
+        self.draggable = MoveCamera(self.camera)
+        self.draggable.start(np.array((event.x, event.y)))
+        self.render()
+
+    def on_puzzle_drag(self, event):
+        if self.draggable:
+            self.draggable.drag(np.array((event.x, event.y)))
+            self.render()
+
+    def on_puzzle_release(self, event):
+        if self.draggable:
+            self.draggable.commit()
+            self.draggable = None
+            self.render()
+
+    def on_puzzle_wheel(self, event):
+        f = pow(1.2, 1 if event.delta > 0 else -1)
+        xy = (event.x, event.y)
+        self.camera.fixed_point_zoom(f, xy)
         self.render()
 
     def on_select_piece(self, event):
         print(f"on_select_piece: {self.var_label.get()}")
 
-    def on_click(self, event):
+    def on_figure_press(self, event):
         e = event
-        if e.inaxes:
-            print(f"on_click: {e.name=} {e.button=} {e.inaxes=} {e.xdata=:.1f} {e.ydata=:.5f}")
+        if not e.inaxes:
+            return
+        
+        x = e.xdata
+        x0 = self.markers[0].get_xdata()[0]
+        x1 = self.markers[1].get_xdata()[0]
+        i = 0 if abs(x - x0) < abs(x - x1) else 1
+        self.markers[i].set_xdata([x])
 
-    def on_motion(self, event):
+        self.dst_points[i] = int(x)
+        self.src_points[i] = int(x) - self.var_offset.get()
+
+        self.figure_canvas.draw_idle()
+
+    def on_figure_motion(self, event):
         if event.inaxes:
             self.cursor_xdata = event.xdata
             self.update_cursor()
         
     def potrace(self):
-        self.potrace_path = puzzler.potrace.piece_to_path(self.piece)
+
+        stepsize = self.var_stepsize.get()
+        ds = self.var_ds.get()
+        
+        for label in self.labels:
+            piece = self.pieces[label]
+            path = puzzler.potrace.piece_to_path(piece)
+            rev = label != self.labels[0]
+            self.curves[label] = Curvature(path, stepsize, reverse=rev, ds=ds)
+            
         self.render()
 
-        ds = self.var_ds.get()
-        self.curvature = c = Curvature(self.potrace_path, self.var_stepsize.get(), ds=ds)
-
-        other_label = self.labels[1]
-        other_piece = None
-        for p in self.puzzle.pieces:
-            if p.label == other_label:
-                other_piece = p
-                break
-        assert other_piece is not None
-
-        other_potrace_path = puzzler.potrace.piece_to_path(other_piece)
-        c2 = Curvature(other_potrace_path, self.var_stepsize.get(), reverse=True, ds=ds)
+        c1 = self.curves[self.labels[0]]
+        c2 = self.curves[self.labels[1]]
 
         eps = self.var_epsilon.get()
-        correlation = algorithm_ii(c.curvature, c2.curvature, eps=eps)
-        lags = np.arange(-len(c2.curvature),len(c.curvature))
+        correlation = algorithm_ii(c1.curvature, c2.curvature, eps=eps)
+        lags = np.arange(-len(c2.curvature),len(c1.curvature))
 
         f = self.figure
         for ax in f.axes.copy():
@@ -236,7 +430,10 @@ class MatchTk:
         ax2 = f.add_subplot(3, 1, 2)
         ax3 = f.add_subplot(3, 1, 3)
 
-        ax1.plot(np.arange(1,len(c.curvature)+1), c.curvature, np.arange(1,len(c2.curvature)+1), c2.curvature)
+        offset = self.var_offset.get()
+        
+        ax1.plot(np.arange(1,len(c1.curvature)+1), c1.curvature)
+        ax1.plot(np.arange(1,len(c2.curvature)+1)+offset, c2.curvature)
         ax1.set_ylabel('curvature')
         ax1.grid(True)
 
@@ -245,74 +442,46 @@ class MatchTk:
         ax2.set_xlabel('offset')
         ax2.grid(True)
 
-        c1 = c.curvature
+        c1 = c1.curvature
         c2 = c2.curvature
-        offset = self.var_offset.get()
-        if offset < 0:
-            start1, end1 = 0, min(len(c1), len(c2)+offset)
-        else:
-            start1, end1 = offset, min(len(c1)-offset, len(c2))
-            
+        start1, end1 = max(0, offset), min(len(c1), len(c2)+offset)
         start2, end2 = start1-offset, end1-offset
 
         c3 = np.abs(c1[start1:end1] - c2[start2:end2])
-        ax3.plot(np.arange(start1,end1), c3)
+        ax3.plot(np.arange(start1,end1), c3, color='tab:green')
         ax3.set_ylabel('abs(A - B)')
         ax3.grid(True)
+
+        self.markers = [ax3.plot([x], [0], marker='o', color='red')[0] for x in (start1, end1-1)]
 
         # self.mc = matplotlib.widgets.MultiCursor(self.figure_canvas, (ax1, ax2, ax3), color='blue', linestyle='--', linewidth=1.)
 
         self.figure_canvas.draw()
 
-    def get_camera_matrix(self):
-
-        t = puzzler.render.Transform()
-        
-        canvas_w = int(self.puzzle_canvas.configure('width')[4])
-        canvas_h = int(self.puzzle_canvas.configure('height')[4])
-        
-        camera_matrix = np.array(
-            ((1,  0,   0),
-             (0, -1, canvas_h-1),
-             (0,  0,   1)), dtype=np.float64)
-        t.multiply(camera_matrix)
-
-        bbox = self.piece.bbox
-        piece_w, piece_h = bbox[1] - bbox[0]
-
-        scale = 0.95 * min(canvas_w / piece_w, canvas_h / piece_h)
-        if scale < 1:
-            t.scale(scale)
-
-        t.translate(0.5 * (np.array((canvas_w, canvas_h)) - (bbox[0] + bbox[1])))
-
-        return t.matrix
-
     def render(self):
         canvas = self.puzzle_canvas
         canvas.delete('all')
 
+        colors = list(matplotlib.colors.TABLEAU_COLORS.values())
+
         r = puzzler.renderer.canvas.CanvasRenderer(canvas)
 
-        r.transform(self.get_camera_matrix())
+        r.transform(self.camera.matrix)
 
-        if self.var_render_perimeter.get():
-            r.draw_lines(self.piece.points, width=1, fill='black')
+        for i, label in enumerate(self.labels):
 
-        if self.potrace_path is not None:
-
-            interp_path = puzzler.potrace.InterpolatePath(self.var_stepsize.get()).apply(self.potrace_path)
+            piece = self.pieces[label]
+            coord = self.coords[label]
             
-            if self.var_render_potrace_path.get():
-                r.draw_lines(interp_path, fill='cyan', width='1')
+            with puzzler.render.save(r):
+            
+                r.translate(coord.dxdy)
+                r.rotate(coord.angle)
 
-            if self.var_render_potrace_lines.get():
-                for i in self.potrace_path:
-                    if isinstance(i,puzzler.potrace.Line):
-                        r.draw_lines(np.array([i.v0, i.v1]), width=3, fill='pink')
+                outline = colors[i % len(colors)]
+                r.draw_polygon(piece.points, outline=outline, fill='', width=2)
 
-            if self.var_render_potrace_points.get():
-                r.draw_points(np.array(interp_path), radius=1, fill='green', outline='')
+                r.draw_text(np.array((0,0)), label)
 
         self.update_cursor()
 
@@ -323,11 +492,16 @@ class MatchTk:
         if self.cursor_xdata is None:
             return
 
-        c = self.curvature.interp_point(self.cursor_xdata)
-        r = puzzler.renderer.canvas.CanvasRenderer(canvas)
-        r.transform(self.get_camera_matrix())
-        r.draw_circle(c, radius=6, fill='', outline='red', tag='cursor')
-
+        for l in self.labels:
+            o = 0 if l == self.labels[0] else self.var_offset.get()
+            c = self.curves[l].interp_point(self.cursor_xdata - o)
+            r = puzzler.renderer.canvas.CanvasRenderer(canvas)
+            r.transform(self.camera.matrix)
+            coord = self.coords[l]
+            r.translate(coord.dxdy)
+            r.rotate(coord.angle)
+            r.draw_circle(c, radius=6, fill='', outline='red', tag='cursor')
+    
 def match(args):
 
     puzzle = puzzler.file.load(args.puzzle)
