3f2cd2a85e72417c57d65abdb41592fd0aaf60ad
Matthew Eldridge <matthew.eldridge@gmail.com>
2024-06-23 10:36:21 -0700

    Scenegraph support

--- lib/puzzler/commands/browse.py
+++ lib/puzzler/commands/browse.py
@@ -1,7 +1,9 @@
+import ctypes
 import cv2 as cv
 import math
 import numpy as np
 import puzzler
+import puzzler.scenegraph
 import puzzler.renderer.canvas
 import puzzler.renderer.cairo
 import puzzler.renderer.opengl
@@ -12,6 +14,18 @@ from tkinter import ttk
 
 # Camera = puzzler.commands.align.Camera
 # MoveCamera = puzzler.commands.align.MoveCamera
+
+def set_process_dpi_awareness():
+
+    awareness = ctypes.c_int()
+    err = ctypes.windll.shcore.GetProcessDpiAwareness(0, ctypes.byref(awareness))
+    if err == 0 and awareness.value == 0:
+        err = ctypes.windll.shcore.SetProcessDpiAwareness(2)
+        
+def simplify_polygon(points, epsilon):
+    approx  = cv.approxPolyDP(points, epsilon, True)
+    poly = np.squeeze(approx)
+    return np.concatenate((poly, poly[:1,:]))
 
 class Browser:
 
@@ -29,7 +43,7 @@ class Browser:
             self.bbox   = tuple(ll.tolist() + ur.tolist())
             self.piece  = piece
 
-    def __init__(self, puzzle, renderer):
+    def __init__(self, puzzle, renderer, use_scenegraph, screensize=None):
 
         self.renderer = renderer
         
@@ -58,8 +72,13 @@ class Browser:
         bbox_w = max(o.bbox[2] - o.bbox[0] for o in self.outlines)
         bbox_h = max(o.bbox[3] - o.bbox[1] for o in self.outlines)
 
-        max_w = 2500 # 1400
-        max_h = 1500 # 800
+        if screensize is not None:
+            max_w, max_h = screensize
+            max_w -= 100
+            max_h -= 200
+        else:
+            max_w = 2500 # 1400
+            max_h = 1500 # 800
 
         if False:
 
@@ -89,6 +108,14 @@ class Browser:
         self.font = None
         self.font2 = None
 
+        if use_scenegraph:
+            self.scenegraph = self.sg_render()
+            # with open('scenegraph_foo.json','w') as f:
+            #     f.write(puzzler.scenegraph.to_json(self.scenegraph))
+        else:
+            self.scenegraph = None
+        self.hittester = None
+
     def render(self, canvas, camera):
 
         if self.renderer == 'opengl':
@@ -100,6 +127,15 @@ class Browser:
             r = puzzler.renderer.canvas.CanvasRenderer(canvas)
 
         r.transform(camera.matrix)
+
+        if self.scenegraph:
+            viewport = (canvas.winfo_width(), canvas.winfo_height())
+            
+            self.hittester = puzzler.scenegraph.BuildHitTester(camera.matrix, viewport)(self.scenegraph.root_node)
+
+            sgr = puzzler.scenegraph.SceneGraphRenderer(r, viewport, scale=camera.zoom)
+            self.scenegraph.root_node.accept(sgr)
+            return r.commit()
 
         self.font = r.make_font("Courier New", 18)
         self.font2 = r.make_font("Courier New", 12)
@@ -140,11 +176,70 @@ class Browser:
 
         r.draw_text(np.zeros(2), text=p.label, font=self.font, fill='black')
 
+    def sg_render(self):
+
+        builder = puzzler.scenegraph.SceneGraphBuilder()
+
+        self.font = ("Courier New", 18)
+        self.font2 = ("Courier New", 12)
+
+        for i, o in enumerate(self.outlines):
+            x = o.col
+            y = self.rows - 1 - o.row
+            tx = (x + .5) * self.bbox_w
+            ty = (y + .5) * self.bbox_h
+            with puzzler.render.save(builder):
+                builder.add_translate((tx, ty))
+                self.sg_outline(builder, o)
+
+        return builder.commit(None, None)
+
+    def sg_outline(self, builder, o):
+
+        p = o.piece
+
+        # want the corners of the outline bbox centered within the tile
+        bbox_center = np.array((o.bbox[0]+o.bbox[2], o.bbox[1]+o.bbox[3])) / 2
+
+        with puzzler.render.save(builder):
+            builder.add_translate(-bbox_center)
+
+            bbox = puzzler.scenegraph.compute_bounding_box(p.points)
+            with puzzler.scenegraph.insert_boundingbox(builder, bbox):
+                self.sg_outline_helper(builder, o)
+
+        builder.add_text(np.zeros(2), text=p.label, font=self.font, fill='black')
+
+    def sg_outline_helper(self, builder, o):
+
+        p = o.piece
+
+        if p.tabs is not None:
+            for i, tab in enumerate(p.tabs):
+                e = tab.ellipse
+                builder.add_ellipse(e.center, e.semi_major, e.semi_minor, e.phi, fill='cyan', outline='')
+                builder.add_text(e.center, text=str(i), font=self.font2, fill='darkblue')
+
+        if p.edges is not None:
+            for edge in p.edges:
+                builder.add_lines(edge.line.pts, width=4, fill='pink')
+
+        props = {'outline':'black', 'fill':'', 'width':1, 'tags':(p.label,)}
+        if True:
+            scales = [0.2, 0.06]
+            nodes = [builder.make_polygon(simplify_polygon(p.points, 0), **props),
+                     builder.make_polygon(simplify_polygon(p.points, 2), **props),
+                     builder.make_polygon(simplify_polygon(p.points, 5), **props)]
+            builder.add_levelofdetail(scales, nodes)
+        else:
+            builder.add_polygon(o.poly, **props)
+
+        
 class BrowseTk:
 
-    def __init__(self, parent, puzzle, renderer):
+    def __init__(self, parent, puzzle, renderer, use_scenegraph, screensize):
 
-        self.browser = Browser(puzzle, renderer)
+        self.browser = Browser(puzzle, renderer, use_scenegraph, screensize)
 
         w, h = self.browser.width, self.browser.height
 
@@ -171,6 +266,24 @@ class BrowseTk:
         self.canvas.bind("<B1-Motion>", self.canvas_drag)
         self.canvas.bind("<ButtonRelease-1>", self.canvas_release)
         self.canvas.bind("<Configure>", self.canvas_configure)
+        self.canvas.bind("<Motion>", self.canvas_motion)
+
+        self.controls = ttk.Frame(self.frame)
+        self.controls.grid(row=1, sticky=(W,E))
+
+        label0 = ttk.Label(self.controls, text="Scale:")
+        label0.grid(column=0, row=0, sticky=(E,))
+
+        self.var_scale = StringVar(value="scale")
+        label1 = ttk.Label(self.controls, textvariable=self.var_scale, width=12)
+        label1.grid(column=1, row=0, sticky=(E,))
+
+        label2 = ttk.Label(self.controls, text="Mouse:")
+        label2.grid(column=2, row=0, sticky=(E,))
+
+        self.var_mouse = StringVar(value="this is my label")
+        label3 = ttk.Label(self.controls, textvariable=self.var_mouse, width=80)
+        label3.grid(column=3, row=0, sticky=(E,))
 
         center = np.array((w / 2, h / 2), dtype=np.float64)
         zoom = self.browser.scale
@@ -183,19 +296,29 @@ class BrowseTk:
         self.camera.viewport = (event.width, event.height)
         if self.browser.renderer != 'opengl':
             self.render()
+        self.update_scale()
 
     def mouse_wheel(self, event):
         f = pow(1.2, 1 if event.delta > 0 else -1)
         xy = (event.x, event.y)
         self.camera.fixed_point_zoom(f, xy)
         self.render()
+        self.update_scale()
+
+    def update_scale(self):
+        scale = self.camera.zoom
+        if scale < 1:
+            scale = f"1/{1/scale:.1f}x"
+        else:
+            scale = f"{scale:.2f}x"
+        self.var_scale.set(scale)
 
     def canvas_press(self, event):
 
         self.draggable = puzzler.commands.align.MoveCamera(self.camera)
         self.draggable.start(np.array((event.x, event.y)))
         self.render()
-        
+
     def canvas_drag(self, event):
 
         if self.draggable:
@@ -209,16 +332,25 @@ class BrowseTk:
             self.draggable = None
             self.render()
 
+    def canvas_motion(self, event):
+
+        if self.browser.hittester:
+            hits = self.browser.hittester((event.x, event.y))
+            s = '; '.join(str(id)+':'+','.join(tags) for id, tags in hits)
+            self.var_mouse.set(s)
+
     def render(self):
 
         self.displayed_image = self.browser.render(self.canvas, self.camera)
 
 def browse(args):
 
+    set_process_dpi_awareness()
     puzzle = puzzler.file.load(args.puzzle)
     
     root = Tk()
-    ui = BrowseTk(root, puzzle, args.renderer)
+    screensize = (root.winfo_screenwidth(), root.winfo_screenheight())
+    ui = BrowseTk(root, puzzle, args.renderer, args.scenegraph, screensize)
     root.bind('<Key-Escape>', lambda e: root.destroy())
     root.title("Puzzler: browse")
     root.mainloop()
@@ -231,4 +363,5 @@ def add_parser(commands):
                                default='tk', help="use opengl rendering (default: tk)")
     parser_browse.add_argument("-t", "--tk",  dest='renderer', action='store_const', const='tk',
                                default='tk', help="use tk rendering (default: tk)")
+    parser_browse.add_argument("-s", "--scenegraph", action='store_true', help="use scenegraph")
     parser_browse.set_defaults(func=browse)
