7c84f1543ecd0035e2ff077ee0eed5164c2d08a8
Matthew Eldridge <matthew.eldridge@gmail.com>
2024-07-23 14:36:43 -0700

    Be more forgiving when fitting triples

--- lib/puzzler/commands/quads.py
+++ lib/puzzler/commands/quads.py
@@ -225,58 +225,64 @@ def try_triples(pieces, quad):
         new_raft = puzzler.raft.Raft(coords, frontiers)
         return raftinator.refine_alignment_within_raft(new_raft)
 
-    def get_fit_and_drop_features(feature_string, drop_label):
+    def find_nearest_tab(raft, dst_label, src_label):
+        src_coord = raft.coords[src_label]
+        dst_coord = raft.coords[dst_label]
+        dst_piece = pieces[dst_label]
 
-        fit = []
-        drop = []
-        for a, b in raftinator.parse_feature_pairs(feature_string):
-            if a.piece == drop_label:
-                drop.append(a)
-                fit.append(b)
-            if b.piece == drop_label:
-                drop.append(b)
-                fit.append(a)
+        dst_src_vec = puzzler.math.unit_vector(src_coord.xy - dst_coord.xy)
 
-        assert len(drop) in (1,2)
+        best_dp = 0.7 # set a floor on how close things need to be
+        nearest_tab = None
         
-        if len(drop) == 2:
-            drop_piece = pieces[drop_label]
-            n = len(drop_piece.tabs)
-            a, b = drop[0].index, drop[1].index
-            if a + 1 == b or a + 1 == n and b == 0 and n > 2:
-                do_reverse = False
-            elif b + 1 == a or b + 1 == n and a == 0 and n > 2:
-                do_reverse = True
-            else:
-                raise ValueError(f"tabs not adjacent: {feature_string=} {drop_label=}")
-            if do_reverse:
-                fit = fit[::-1]
-                drop = drop[::-1]
+        for dst_tab_no, dst_tab in enumerate(dst_piece.tabs):
+            dst_tab_vec = puzzler.math.unit_vector(dst_coord.xform.apply_n2(dst_tab.ellipse.center))
+            dp = np.sum(dst_src_vec * dst_tab_vec)
 
-        return fit, drop
+            if best_dp < dp:
+                assert nearest_tab is None 
+                best_dp = dp
+                nearest_tab = (dst_label, dst_tab_no, dst_tab.indent)
 
-    def make_feature_pairs(try_piece, try_tab_pair, fit_features):
+        return nearest_tab
+            
+    def get_inside_corner_tab_pair(raft, quad, drop_quadrant):
 
-        tab_a, tab_b = try_tab_pair
+        label_order = ['ul_piece', 'ur_piece', 'lr_piece', 'll_piece']
+        i = label_order.index(drop_quadrant)
 
-        if tab_a is None:
+        curr_label = quad[drop_quadrant]
+        prev_label = quad[label_order[i-1]]
+        next_label = quad[label_order[i-3]]
+
+        prev_tab = find_nearest_tab(raft, prev_label, curr_label)
+        next_tab = find_nearest_tab(raft, next_label, curr_label)
+
+        # print(f"raft={quad['raft']} prev={prev_label} curr={curr_label} next={next_label} retval={(prev_tab, next_tab)}")
+        return (prev_tab, next_tab)
+
+    def make_feature_pair(a, b):
+        if a is None or b is None:
             return None
+        if a[2] == b[2]:
+            raise ValueError("tab conflict")
+        return (Feature(a[0],'tab',a[1]), Feature(b[0],'tab',b[1]))
 
-        if len(fit_features) == 2 and tab_b is None:
-            return None
+    def make_feature_pairs(dst_tab_pair, src_tab_pair):
+        
+        retval = []
+        try:
+            fp = make_feature_pair(dst_tab_pair[0], src_tab_pair[1])
+            if fp is not None:
+                retval.append(fp)
+            fp = make_feature_pair(dst_tab_pair[1], src_tab_pair[0])
+            if fp is not None:
+                retval.append(fp)
+            
+        except ValueError:
+            retval = []
 
-        if len(fit_features) == 1 and tab_b is not None:
-            return None
-
-        for i, f in enumerate(fit_features):
-            if try_tab_pair[i][2] == pieces[f.piece].tabs[f.index].indent:
-                return None
-
-        try_features = [Feature(try_piece.label, 'tab', tab_a[1])]
-        if tab_b is not None:
-            try_features.append(Feature(try_piece.label, 'tab', tab_b[1]))
-
-        return list(zip(fit_features, try_features))
+        return retval
 
     debug = False
 
@@ -288,21 +294,27 @@ def try_triples(pieces, quad):
 
     retval2 = []
 
+    quad_no = quad['quad_no']
+
     for drop_quadrant in 'ul_piece', 'ur_piece', 'll_piece', 'lr_piece':
 
         drop_label = quad[drop_quadrant]
 
         # HACK
-        if debug and drop_label != 'B2':
+        if debug and drop_label != 'B11':
             continue
 
         triple_raft = remove_piece_from_raft(good_raft, drop_label)
 
-        fit_features, drop_features = get_fit_and_drop_features(quad['raft'], drop_label)
+        fit_tab_pair = get_inside_corner_tab_pair(good_raft, quad, drop_quadrant)
 
-        # HACK
+        triple_feature_pairs = []
+        for a, b in raftinator.parse_feature_pairs(quad['raft']):
+            if a.piece != drop_label and b.piece != drop_label:
+                triple_feature_pairs.append((a,b))
+                
         if debug:
-            print(f"{drop_label=} {fit_features=}")
+            print(f"{drop_label=} {fit_tab_pair=}")
 
         retval = []
 
@@ -312,60 +324,44 @@ def try_triples(pieces, quad):
                 continue
 
             # HACK
-            if debug and try_label not in ('B2', 'G17'):
+            if debug and try_label not in ('AA12', 'B11'):
                 continue
 
             try_raft = raftinator.factory.make_raft_for_piece(try_label)
 
             for try_tab_pair in tab_pairs_for_piece(try_piece):
 
-                # tab_pairs_for_piece() returns tab pairs in CCW order
-                # (nice job Matt) while tabs are number in CW order,
-                # and we've chosen to order the feature pairs in CW
-                # order for consistency.  In other words, reverse the
-                # tab pairs now...
-                try_tab_pair = try_tab_pair[1], try_tab_pair[0]
-                
-                if debug:
-                    print(f"  {try_tab_pair=}")
-
-                feature_pairs = make_feature_pairs(try_piece, try_tab_pair, fit_features)
-                if feature_pairs is None:
-                    if debug:
-                        print("  -- not valid")
+                try_feature_pairs = make_feature_pairs(fit_tab_pair, try_tab_pair)
+                if len(try_feature_pairs) < 1:
                     continue
 
                 if debug:
-                    print(f"  {feature_pairs=}")
+                    print(f"  {try_feature_pairs=}")
 
                 new_raft = raftinator.align_and_merge_rafts_with_feature_pairs(
-                    triple_raft, try_raft, feature_pairs)
+                    triple_raft, try_raft, try_feature_pairs)
 
+                init_mse = raftinator.get_total_error_for_raft_and_seams(new_raft)
+                
                 for _ in range(3):
                     new_raft = raftinator.refine_alignment_within_raft(new_raft)
 
                 mse = raftinator.get_total_error_for_raft_and_seams(new_raft)
 
-                fdict = dict(feature_pairs)
-                
-                desc = raftinator.parse_feature_pairs(quad['raft'])
-                for i in range(len(desc)):
-                    a, b = desc[i]
-                    if a in fdict:
-                        desc[i] = (a, fdict[a])
-                    elif b in fdict:
-                        desc[i] = (fdict[b], b)
-                desc = raftinator.format_feature_pairs(desc)
+                desc = raftinator.format_feature_pairs(triple_feature_pairs + try_feature_pairs)
                 
                 if debug:
                     print(f"  {mse=:.3f} {desc=}")
 
-                row = {'ul_piece': quad['ul_piece'],
+                row = {'quad_no': quad_no,
+                       'ul_piece': quad['ul_piece'],
                        'ur_piece': quad['ur_piece'],
                        'll_piece': quad['ll_piece'],
                        'lr_piece': quad['lr_piece'],
+                       'drop_piece': drop_label,
                        'fit_piece': try_label,
                        'raft': desc,
+                       'init_mse': init_mse,
                        'mse': mse,
                        'rank': None}
                 row[drop_quadrant] = None
@@ -375,9 +371,11 @@ def try_triples(pieces, quad):
         retval.sort(key=operator.itemgetter('mse'))
         for i, row in enumerate(retval, start=1):
             row['rank'] = i
+            row['init_mse'] = decimal.Decimal(f"{row['init_mse']:.3f}")
             row['mse'] = decimal.Decimal(f"{row['mse']:.3f}")
 
         retval2 += retval
+        quad_no += 1
 
     return retval2
 
@@ -417,16 +415,20 @@ def triplets(args):
         reader = csv.DictReader(ifile)
         for row in reader:
             if int(row['rank']) == 1:
+                # 4x because we'll really do 4 separately ranked experiments per quad.  Gross.
+                row['quad_no'] = 4 * len(quads)
                 quads.append(row)
 
     with open(output_csv_path, 'w', newline='') as ofile:
-        fieldnames = 'ul_piece ur_piece ll_piece lr_piece fit_piece raft mse rank'
+        fieldnames = 'quad_no ul_piece ur_piece ll_piece lr_piece drop_piece fit_piece raft mse rank'
         writer = csv.DictWriter(ofile, fieldnames=fieldnames.split())
         writer.writeheader()
 
         # HACK
-        if False:
-            writer.writerows(try_triples(pieces, quads[0]))
+        if True:
+            for q in quads:
+                if q['ul_piece'] == 'A11':
+                    writer.writerows(try_triples(pieces, q))
             return
 
         if args.num_workers:
