10fa88450ce3023a60bf3cf58c434ecb8abcd31b
Matthew Eldridge <matthew.eldridge@gmail.com>
2024-08-30 10:09:06 -0700

    Adjustable exposure

--- camera/capture.py
+++ camera/capture.py
@@ -12,12 +12,43 @@ def get_size(cam):
 
 print("Opening camera...")
 
-cam = cv.VideoCapture(1, cv.CAP_MSMF)
+cam = cv.VideoCapture(2, cv.CAP_MSMF)
+# cam = cv.VideoCapture(3, cv.CAP_DSHOW)
+
+for k in dir(cv):
+    if not k.startswith('CAP_PROP'):
+        continue
+    if k.startswith('CAP_PROP_AUDIO_'):
+        continue
+    if k.startswith('CAP_PROP_DC1394_'):
+        continue
+    if k.startswith('CAP_PROP_GPHOTO2_'):
+        continue
+    if k.startswith('CAP_PROP_GIGA_'):
+        continue
+    if k.startswith('CAP_PROP_INTELPERC_'):
+        continue
+    if k.startswith('CAP_PROP_IOS_'):
+        continue
+    if k.startswith('CAP_PROP_OBSENSOR'):
+        continue
+    if k.startswith('CAP_PROP_OPENNI'):
+        continue
+    if k.startswith('CAP_PROP_PVAPI_'):
+        continue
+    if k.startswith('CAP_PROP_XI_'):
+        continue
+    
+    v = cam.get(getattr(cv,k))
+    print(f"{k}: {v}")
+
+print("Frame size:", get_size(cam))
 
 w, h = 4656, 3496
 set_size(cam, w, h)
 
-print("Frame size:", get_size(cam))
+w, h = get_size(cam)
+print("Frame size:", (w, h))
 
 print("Backend:", cam.getBackendName())
 
@@ -28,6 +59,7 @@ cv.namedWindow("overview")
 cv.namedWindow("detail")
 
 img_counter = 0
+exposure = cam.get(cv.CAP_PROP_EXPOSURE)
 
 while True:
     ret, frame = cam.read()
@@ -49,6 +81,14 @@ while True:
         cv.imwrite(img_name, frame)
         print("{} written!".format(img_name))
         img_counter += 1
+    elif k % 256 == ord('<'):
+        exposure -= 1
+        cam.set(cv.CAP_PROP_EXPOSURE, exposure)
+        print(f"{exposure=}")
+    elif k % 256 == ord('>'):
+        exposure += 1
+        cam.set(cv.CAP_PROP_EXPOSURE, exposure)
+        print(f"{exposure=}")
 
 cam.release()
 
