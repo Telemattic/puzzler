ce4e09ca4165764eb04d232a6efebb0aa34e3917
Matthew Eldridge <matthew.eldridge@gmail.com>
2024-10-17 07:25:16 -0700

    Refactor

--- camera/ical.py
+++ camera/ical.py
@@ -17,8 +17,68 @@ def compute_contour_center_of_mass(contour):
     x = int(m['m10']/m['m00'])
     y = int(m['m01']/m['m00'])
     return (x, y)
-    
-def choose_contour_center(contour, dpath):
+
+class ContourDistanceImage:
+
+    def __init__(self, contour, finger_radius_px):
+        self.contour = contour
+        self.finger_radius_px = finger_radius_px
+        
+        self.ll = np.min(contour, axis=0) - 5
+        self.ur = np.max(contour, axis=0) + 5
+        w, h = self.ur + 1 - self.ll
+
+        # construct a new vector of points offset by ll, do _not_ modify
+        # the existing points in place, this is purely local bookkeeping
+        pp = self.contour - self.ll
+
+        piece_image = np.ones((h, w), dtype=np.uint8)
+        piece_image[pp[:,1], pp[:,0]] = 0
+
+        piece_image = cv.floodFill(piece_image, None, (0,0), 0)[1]
+
+        self.di = cv.distanceTransform(piece_image, cv.DIST_L2, cv.DIST_MASK_PRECISE)
+
+    def optimize_center(self, center):
+        di = self.di
+        h, w = di.shape
+        dx2 = np.square(np.arange(w) - (center[0] - self.ll[0]))
+        dy2 = np.square(np.arange(h) - (center[1] - self.ll[1]))
+        center_di = np.sqrt(np.atleast_2d(dx2) + np.atleast_2d(dy2).T)
+
+        opt_index = np.argmin(np.where(di >= self.finger_radius_px, center_di, np.inf))
+
+        # reverse the unraveled index to change from (y,x) to (x,y) tuple
+        opt_center = np.unravel_index(opt_index, di.shape)[::-1] + self.ll
+
+        # if the piece is so small that there are no safe points within it
+        # then we get a meaningless answer, make sure we don't blindly
+        # accept it
+        dist_to_edge = cv.pointPolygonTest(self.contour, np.float32(opt_center), True)
+        assert dist_to_edge >= self.finger_radius_px
+
+        return opt_center
+
+    def make_color_image(self, center, optimized_center):
+
+        di, ll = self.di, self.ll
+
+        finger_okay = np.uint8(np.where(di >= self.finger_radius_px, 255, 0))
+        finger_contours = cv.findContours(finger_okay, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)[0]
+
+        img = np.uint8(di * (255 / np.max(di)))
+        img = cv.cvtColor(np.uint8(cm.afmhot(img) * 255), cv.COLOR_RGBA2BGR)
+
+        cv.drawContours(img, [self.contour-ll], 0, (127,0,127), thickness=2, lineType=cv.LINE_AA)
+        cv.drawContours(img, finger_contours, -1, (0,127,0), thickness=2, lineType=cv.LINE_AA)
+        if center is not None:
+            cv.circle(img, center-ll, 9, (255, 0, 127), thickness=2, lineType=cv.LINE_AA)
+        if optimized_center is not None:
+            cv.circle(img, optimized_center-ll, 9, (127, 255, 0), thickness=2, lineType=cv.LINE_AA)
+
+        return img
+        
+def choose_contour_center(contour, dpath = None):
 
     dpi = 600.
     finger_diameter_mm = 11.7
@@ -36,58 +96,19 @@ def choose_contour_center(contour, dpath):
         return center
 
     # center of mass is too close to the edge of the piece, find the
-    # point closest to the center of mass isn't too close to an edge
-    
-    ll = np.min(contour, axis=0) - 5
-    ur = np.max(contour, axis=0) + 5
-    w, h = ur + 1 - ll
+    # point closest to the center of mass that isn't too close to an
+    # edge
 
-    # construct a new vector of points offset by ll, do _not_ modify
-    # the existing points in place, this is purely local bookkeeping
-    pp = contour - ll
+    cdi = ContourDistanceImage(contour, finger_radius_px)
 
-    piece_image = np.ones((h, w), dtype=np.uint8)
-    piece_image[pp[:,1], pp[:,0]] = 0
+    optimized_center = cdi.optimize_center(center)
 
-    piece_image = cv.floodFill(piece_image, None, (0,0), 0)[1]
-    
-    di = cv.distanceTransform(piece_image, cv.DIST_L2, cv.DIST_MASK_PRECISE)
+    if dpath:
+        img = cdi.make_color_image(center, optimized_center)
+        print(f"Saving distance to {dpath}")
+        cv.imwrite(dpath, img)
 
-    dx2 = np.square(np.arange(w) - (center[0] - ll[0]))
-    dy2 = np.square(np.arange(h) - (center[1] - ll[1]))
-    di2 = np.sqrt(np.atleast_2d(dx2) + np.atleast_2d(dy2).T)
-    assert di.shape == di2.shape
-
-    # derp = cm.afmhot(di2 / np.max(di2))
-    # derp = cv.cvtColor(np.uint8(derp * 255), cv.COLOR_RGBA2BGR)
-    # derp_path = dpath.replace('_dist.', '_derp.')
-    # cv.imwrite(derp_path, derp)
-
-    optimized_center = np.unravel_index(np.argmin(np.where(di >= finger_radius_px, di2, np.inf)), di.shape)
-    optimized_center = optimized_center[::-1]
-
-    # if the piece is so small that there are no safe points within it
-    # then we get a meaningless answer, make sure we don't blindly
-    # accept it
-    assert cv.pointPolygonTest(contour, np.float32(optimized_center+ll), True) >= finger_radius_px
-    
-    finger_okay = np.uint8(np.where(di >= finger_radius_px, 255, 0))
-    finger_contours = cv.findContours(finger_okay, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)[0]
-
-    s = 255 / np.max(di)
-    di8 = np.array(di * s, dtype=np.uint8)
-
-    di8 = cv.cvtColor(np.uint8(cm.afmhot(di8) * 255), cv.COLOR_RGBA2BGR)
-    
-    cv.drawContours(di8, [contour-ll], 0, (127,0,127), thickness=2, lineType=cv.LINE_AA)
-    cv.drawContours(di8, finger_contours, -1, (0,127,0), thickness=2, lineType=cv.LINE_AA)
-    cv.circle(di8, center-ll, 9, (255, 0, 127), thickness=2, lineType=cv.LINE_AA)
-    cv.circle(di8, optimized_center, 9, (127, 255, 0), thickness=2, lineType=cv.LINE_AA)
-
-    print(f"Saving distance to {dpath}")
-    cv.imwrite(dpath, di8)
-
-    return optimized_center + ll
+    return optimized_center
     
 def get_points_for_piece(ipath, dpath, opath):
 
@@ -95,17 +116,12 @@ def get_points_for_piece(ipath, dpath, opath):
     img = cv.imread(ipath, cv.IMREAD_COLOR)
     contour = PerimeterComputer(img, threshold=75).contour
     
-    m = cv.moments(contour)
-    cx = int(m['m10']/m['m00'])
-    cy = int(m['m01']/m['m00'])
-
     cp = choose_contour_center(np.squeeze(contour), dpath)
 
     print(f"Saving piece to {opath}")
     img = np.copy(np.flip(img, axis=0))
     cv.drawContours(img, [contour], 0, (0,0,255), thickness=2, lineType=cv.LINE_AA)
-    cv.circle(img, (cx,cy), 6, (255,255,0), thickness=2, lineType=cv.LINE_AA)
-    cv.circle(img, cp, 9, (255, 0, 127), thickness=2, lineType=cv.LINE_AA)
+    cv.circle(img, cp, 9, (127, 255, 0), thickness=2, lineType=cv.LINE_AA)
     img = np.copy(np.flip(img, axis=0))
 
     if os.path.exists(opath):
@@ -121,7 +137,7 @@ def main():
             opath = f"piece_{label}_out.jpg"
             for i in (dpath, opath):
                 if os.path.exists(i):
-                    os.unlink(i)
+                    os.remove(i)
             get_points_for_piece(ipath, dpath, opath)
             print('-' * 70)
 
